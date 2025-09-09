815e20ee843a6f38a69bee1489f777da97f035a7
Safa-abdulhakim <safaabd2025@gmail.com>
2025-09-10 00:23:17 +0300

    Add build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/Safa_Studio/',
   plugins: [react(), svgr()],
   server: {
     port: 3000,

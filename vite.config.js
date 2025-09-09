7da2f603990f624c03c36adbdc74625fecad5810
Safa-abdulhakim <safaabd2025@gmail.com>
2025-09-10 00:28:18 +0300

    Update project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from '@svgr/rollup';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Safa_Studio/',
-  plugins: [react(), svgr()],
-  server: {
-    port: 3000,
-  }
+  plugins: [react()],
+  base: '/Safa_Studio/' // ضع اسم المستودع هنا
 })
